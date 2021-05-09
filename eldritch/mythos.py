9624ce798b39b1ddb999989fe82fba1eb0f6352f
Peter Combs <peter.combs@invitae.com>
2021-05-08 23:26:08 -0700

    Implementable Mythos

--- eldritch/mythos.py
+++ eldritch/mythos.py
@@ -1,4 +1,5 @@
 import eldritch.events as events
+import eldritch.places as places
 
 
 class GlobalEffect(object):
@@ -135,10 +136,11 @@ class Mythos6(Environment):
     super(Mythos6, self).__init__("Mythos6", "Graveyard", "Isle", {"plus"}, {"moon"}, "weather")
 
   def get_modifier(self, thing, attribute):
-    if attribute == "will":
-      return -1
-    if attribute == "sneak":
-      return 1
+    if isinstance(thing.place, places.CityPlace):
+      if attribute == "will":
+        return -1
+      if attribute == "sneak":
+        return 1
     return 0
 
   def get_interrupt(self, event, owner, state):
@@ -146,17 +148,136 @@ class Mythos6(Environment):
       return None  # TODO: prevent movement
     return None
 
+class Mythos7(Headline):
+  def __init__(self):
+    super(Mythos7, self).__init__(
+        "Mythos7", "Unnamable", "Woods", {"hexagon"}, {"slash", "triangle", "star"}
+    )
+  #TODO: All players sprung from jail, cannot be arrested
+
+class Mythos8(Environment):
+  def __init__(self):
+    super(Mythos8, self).__init__(
+        "Mythos8", "Square", "Unnamable", {"square", "diamond"}, {"circle"}, "mystic"
+    )
+  #TODO: Rivertown blood magic, maybe devoured
+
+
+class Mythos9(Environment):
+  def __init__(self):
+    super(Mythos9, self).__init__(
+        "Mythos9", "Cave", "Roadhouse", {"square", "diamond"}, {"circle"}, "mystic"
+    )
+
+  def get_modifier(self, thing, attribute):
+    if attribute == "luck":
+      return -1
+    if attribute == "sneak":
+      return 1
+    return 0
+
+
+class Mythos10(Headline):
+  def __init__(self):
+    super(Mythos10, self).__init__(
+        "Mythos10", "Isle", "Science", {"hexagon"}, {"slash", "triangle", "star"}
+    )
+  #TODO: Close stores
+
 
 class Mythos11(Headline):
 
   def __init__(self):
     super(Mythos11, self).__init__(
-        "Mythos11", "Cave", "Roadhouse", {"hex"}, {"slash", "triangle", "star"})
+        "Mythos11", "Cave", "Roadhouse", {"hex"}, {"slash", "triangle", "star"}
+    )
 
   def create_event(self, state):
     seq = super(Mythos11, self).create_event(state)
     seq.events.append(events.ReturnToCup(places={"Southside", "House", "Church", "Society"}))
     return seq
+
+
+class Mythos12(Headline):
+  def __init__(self):
+    super(Mythos12, self).__init__(
+        "Mythos12", "Square", "Unnamable", {"circle"}, {"square", "diamond"}
+    )
+
+  def create_event(self, state):
+    seq = super(Mythos12, self).create_event(state)
+    seq.events.append(
+        events.ReturnToCup(places={"University", "Administration", "Library", "Science"})
+    )
+    return seq
+
+
+class Mythos14(Environment):
+  def __init__(self):
+    super(Mythos14, self).__init__(
+        "Mythos14", "Unnamable", "Woods", {"square", "diamond"}, {"circle"}, "urban"
+    )
+
+  def get_trigger(self, event, owner, state):
+    char = getattr(event, 'character', None)
+    if isinstance(event, events.EndMovement) and char.place.name == "Northside":
+      gain = events.Gain(char, {'clues': 1})
+      check = events.Check(char, 'will', -1)
+      wonders = events.PassFail(char, check, events.Nothing(), events.Loss(char, {'sanity': 1}))
+      return events.Sequence([gain, wonders])
+
+
+class Mythos15(Environment):
+  def __init__(self):
+    super(Mythos15, self).__init__("Mythos15", "Isle", "Science", {"plus"}, {"moon"})
+
+  def get_trigger(self, event, owner, state):
+    char = getattr(event, 'character', None)
+    if isinstance(event, events.EndMovement) and isinstance(char.place, places.Street):
+      check = events.Check(char, 'will', 0)
+      arrested = events.Arrested(char)
+      return events.PassFail(char, check, events.Nothing, arrested)
+
+
+class Mythos16(Environment):
+  def __init__(self):
+    super(Mythos16, self).__init__("Mythos16", "Witch", "Cave", {"square", "diamond"}, {"circle"}, "urban")
+
+  def get_trigger(self, event, owner, state):
+    char = getattr(event, 'character', None)
+    if isinstance(event, events.EndMovement) and char.place.name == 'Uptown':
+      return events.Purchase(char, 'unique', 2)
+
+
+class Mythos17(Environment):
+  def __init__(self):
+    super(Mythos17, self).__init__("Mythos17", "Witch", "Cave", {"plus"}, {"moon"}, "urban")
+
+  def get_trigger(self, event, owner, state):
+    char = getattr(event, 'character', None)
+    if isinstance(event, events.EndMovement) and char.place.name == "University":
+      check = events.Check(char, "lore", -1)
+      gain = events.Gain(char, {"clues": 1})
+      return events.PassFail(char, check, gain, events.Nothing())
+
+class Mythos18(Environment):
+  def __init__(self):
+    super(Mythos18, self).__init__("Mythos18", "Square", "Unnamable", {"plus"}, {"moon"}, "mystic")
+  #TODO: Investigators cannot gain sanity except by receiving care at asylum or from the Psychologist
+
+
+class Mythos19(Headline):
+  def __init__(self):
+    super(Mythos19, self).__init__("Mythos19", "Witch", "Cave", {"moon"}, {"plus"})
+  #TODO: close merchant district streets (in or out)
+
+
+class Mythos20(Headline):
+  def __init__(self):
+    super(Mythos20, self).__init__("Mythos20", "Cave", "Roadhouse", {"moon"}, {"plus"})
+
+  def create_event(self, state):
+    return events.ReturnToCup('streets')
 
 
 class Mythos45(Environment):
